3f059e41b359ce9811e25f92ca1a21f51ccc19b2
GabrielHaddad <gabrielphaddad@yahoo.com.br>
2022-01-19 14:15:00 -0300

    experimenting grappling hook

---
+++ Assets/Scripts/Rope.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rope : MonoBehaviour
+{
+
+    Rigidbody2D rb2d;
+    SpringJoint2D joint2D;
+    private LineRenderer lineRenderer;
+    private List<Vector2> ropeSegments = new List<Vector2>();
+    private float ropeSegLen = 0.25f;
+    private int segmentLength = 35;
+    private float lineWidth = 0.1f;
+    Vector2 currentGrapplePosition;
+    Vector2 worldPoint;
+    Vector2 grapplePoint;
+    [SerializeField] Transform gunPoint; 
+    BoxCollider2D boxCollider2D;
+    [SerializeField] LayerMask whatIsGrappable;
+    [SerializeField] LayerMask isGround;
+    [SerializeField] float runSpeed;
+    [SerializeField] float jumpForce;
+    [SerializeField] float distanceGrapple;
+    bool isGrapling = false;
+
+    
+    void Awake() 
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        rb2d = GetComponent<Rigidbody2D>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+        joint2D = GetComponent<SpringJoint2D>();
+    }
+
+    void Start()
+    {
+        joint2D.enabled = false;
+        lineRenderer.startWidth = lineWidth;
+        lineRenderer.endWidth = lineWidth;
+        // Vector3 ropeStartPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // for (int i = 0; i < segmentLength; i++)
+        // {
+        //     this.ropeSegments.Add(ropeStartPoint);
+        //     ropeStartPoint.y -= ropeSegLen;
+        // }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartGrapple();
+        }
+
+        Run();
+        
+        if (Input.GetKey(KeyCode.Space) && (boxCollider2D.IsTouchingLayers(isGround) || isGrapling))
+        {
+            rb2d.AddForce(new Vector2 (0f, jumpForce * Time.deltaTime), ForceMode2D.Impulse);
+            isGrapling = false;
+            joint2D.enabled = false;
+            lineRenderer.enabled = false;
+        }
+        
+    }
+
+    void LateUpdate() 
+    {
+        if (isGrapling)
+        {
+            DrawRope();
+            lineRenderer.enabled = true;
+        }
+    }
+
+    void StartGrapple()
+    {
+        worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero, 0f, whatIsGrappable);
+
+        if (hit.collider != null)
+        {
+            isGrapling = true;
+            grapplePoint = hit.point;
+            joint2D.autoConfigureConnectedAnchor = false;
+            joint2D.connectedAnchor = hit.point;
+
+            float distanceFromPoint = Vector2.Distance(gunPoint.position, grapplePoint);
+
+            joint2D.autoConfigureDistance = false;
+            joint2D.distance = distanceFromPoint * distanceGrapple;
+            joint2D.enabled = true;
+
+            lineRenderer.positionCount = 2;
+        }
+    }
+
+    void Run()
+    {
+        rb2d.AddForce( new Vector2(Input.GetAxisRaw("Horizontal") * runSpeed * Time.deltaTime, 0f), ForceMode2D.Impulse);
+    }
+
+    private void DrawRope()
+    {
+        currentGrapplePosition = Vector2.Lerp(worldPoint, gunPoint.position, Time.deltaTime * 8f);
+
+        lineRenderer.SetPosition(0, gunPoint.position);
+        lineRenderer.SetPosition(1, currentGrapplePosition);
+    }
+}
